7e8de866ca106c82c7c354618de7344a0e961b2f
Ethel E. González <ethel@nativo.company>
2020-01-04 01:56:18 -0600

    Se añadio el subnodo indemnización

---
+++ src/CFDI/Node/Complemento/Nomina/Percepcion/DetPercIndenmizacion.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the CFDI project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Charles\CFDI\Node\Complemento\Nomina\Percepcion;
+
+use Charles\CFDI\Common\Node;
+
+/**
+ * This is the Percepcion details class.
+ *
+ * @author Ethel Gonzalez <emmelaine.glz@gmail.com>
+ */
+ class DetPercIndenmizacion extends Node
+ {
+     /**
+      * Node name.
+      *
+      * @var string
+      */
+     protected $parentNodeName = 'nomina12:Percepciones';
+
+     /**
+      * Node name.
+      *
+      * @var string
+      */
+     protected $nodeName = 'nomina12:SeparacionIndemnizacion';
+
+
+     /**
+      * Details Percepcion constructor.
+      * @param array $data
+      */
+     public function __construct(array $data = [])
+     {
+         parent::__construct($data, $this->parentAttributes());
+     }
+
+     /**
+      * Node attributes.
+      *
+      * @return array
+      */
+     public function parentAttributes()
+     {
+         return [];
+     }
+ }
